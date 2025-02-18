5d420533d7a9491c2ab5e04c863329c8fb940d18
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-02-18 16:32:01 +0100

    Update Bets.sql

--- Bets.sql
+++ Bets.sql
@@ -5,27 +5,27 @@ reportingdate														-- Date
 ,CouponID															-- ID for coupons
 ,b.CustomerKey														-- ID for customers -- KEY to DW.Customer
 ,CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
-    CONCAT(
-        COALESCE(b.SegmentKey, ''), '|', 
-        COALESCE(c.CountryKey, '')
-    )), 2) AS key_geo
+CONCAT(
+b.SegmentKey, '|', 
+c.CountryKey)), 2) 
+AS key_geo
 ,CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
-    CONCAT(
-        COALESCE(IsMobileBet, ''), '|', 
-        COALESCE(IsLiveBet, ''), '|',
-        COALESCE(NumberOfSelections, ''), '|',
-        COALESCE(CouponTypeKey, ''), '|',
-        COALESCE(BetSettledStatus, ''), '|',
-        COALESCE(CASE WHEN MAX(BetSelectionTypeKey) > 1 THEN 2 ELSE 1 END, ''), '|',
-        COALESCE(BonusTypeKey, ''), '|',
-        COALESCE(CustomerClassificationAtBetPlacement, ''), '|',
-        COALESCE(BetStakeEURBucketKey, ''), '|',
-        COALESCE(SelectionsBucket, ''), '|',
-        COALESCE(CashoutParticipation, ''), '|',
-        COALESCE(NumberofPartialCashout, '')
-    )), 2) AS key_betdetails
-,CASE WHEN numberofselections=1 AND CouponTypeKey=1 
-	THEN BetGroupKey ELSE 0 END AS BetGroupKey						-- KEY to DW.BetGroup
+CONCAT(
+IsMobileBet, '|', 
+IsLiveBet, '|',
+NumberOfSelections, '|',
+CouponTypeKey, '|',
+BetSettledStatus, '|',
+CASE WHEN MAX(BetSelectionTypeKey) > 1 THEN 2 ELSE 1 END, '|',
+BonusTypeKey, '|',
+CashoutParticipation)), 2) 
+AS key_betdetails
+,CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
+CONCAT(
+BetStakeEURBucketKey, '|', 
+SelectionsBucket)), 2) 
+AS key_betbucket
+,CASE WHEN numberofselections=1 AND CouponTypeKey=1 THEN BetGroupKey ELSE 0 END AS BetGroupKey						-- KEY to DW.BetGroup
 	
 --MEASURES
 	,EXP(SUM(LOG(NULLIF(BetSelectionOdds, 0)))) AS Odds				
@@ -45,8 +45,7 @@ INNER JOIN dw.Customer c WITH(NOLOCK) ON b.customerkey = c.customerkey --WITH(NO
 WHERE 
     betsettledstatus IN (3, 4, 5, 6, 7)
     AND b.AccountTypeKey = 1
-	AND ReportingDate>='2024-11-01'
-	AND ReportingDate<'2025-02-01'
+	AND ReportingDate='2024-11-01'
 
 GROUP BY
 reportingdate														-- Date
@@ -63,8 +62,5 @@ reportingdate														-- Date
 ,IsLiveBet															-- MarketType
 ,SelectionsBucket													-- KEY to DW.BucketSelections
 ,CashoutParticipation												-- KEY to DW.VW_CashoutParticipation
-,NumberofPartialCashout												-- Number of Partial Cashouts
-,CustomerClassificationAtBetPlacement								-- KEY to DW.CustomerAccountClassification
 ,BonusTypeKey														-- KEY to DW.BonusType
-,CASE WHEN numberofselections=1 AND CouponTypeKey=1 
-	THEN BetGroupKey ELSE 0 END
+,CASE WHEN numberofselections=1 AND CouponTypeKey=1 THEN BetGroupKey ELSE 0 END
