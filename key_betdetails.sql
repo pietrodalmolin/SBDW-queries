0a8bde7518eb5b9dccd23f07b38f1c1ede33a4e2
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-02-18 16:28:49 +0100

    Update key_betdetails.sql

--- key_betdetails.sql
+++ key_betdetails.sql
@@ -1,37 +1,37 @@
 SELECT 
-    CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
-        CONCAT(
-            IsMobileBet, '|', 
-            IsLiveBet, '|',
-            NumberOfSelections, '|',
-            CouponTypeKey, '|',
-            BetSettledStatus, '|',
-            BetSelectionTypeKey, '|',
-            BonusTypeKey, '|',
-            CashoutParticipation
-        )
-    ), 2) AS key_betdetails,
-    IsMobileBet,
-    IsLiveBet,
-    NumberOfSelections,
-    CouponTypeKey,
-    BetSettledStatus,
-    BetSelectionTypeKey,
-    BonusTypeKey,
-    CashoutParticipation
+CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
+CONCAT(
+IsMobileBet, '|', 
+IsLiveBet, '|',
+NumberOfSelections, '|',
+CouponTypeKey, '|',
+BetSettledStatus, '|',
+BetSelectionTypeKey, '|',
+BonusTypeKey, '|',
+CashoutParticipation
+)
+), 2) AS key_betdetails,
+IsMobileBet,
+IsLiveBet,
+NumberOfSelections,
+CouponTypeKey,
+BetSettledStatus,
+BetSelectionTypeKey,
+BonusTypeKey,
+CashoutParticipation
 FROM 
-    (SELECT 0 AS IsMobileBet UNION ALL SELECT 1) AS IsMobileBet
+(SELECT 0 AS IsMobileBet UNION ALL SELECT 1) AS IsMobileBet
 CROSS JOIN 
-    (SELECT 0 AS IsLiveBet UNION ALL SELECT 1) AS IsLiveBet
+(SELECT 0 AS IsLiveBet UNION ALL SELECT 1) AS IsLiveBet
 CROSS JOIN 
-    (SELECT 1 AS NumberOfSelections UNION ALL SELECT 2) AS NumberOfSelections
+(SELECT 1 AS NumberOfSelections UNION ALL SELECT 2) AS NumberOfSelections
 CROSS JOIN 
-    (SELECT DISTINCT CouponTypeKey FROM DW.CouponType) AS CouponTypeKey
+(SELECT DISTINCT CouponTypeKey FROM DW.CouponType) AS CouponTypeKey
 CROSS JOIN 
-    (SELECT DISTINCT SettledStatusKey AS BetSettledStatus FROM DW.SettledStatus WHERE SettledStatusKey IN (3, 4, 5, 6, 7)) AS BetSettledStatus
+(SELECT DISTINCT SettledStatusKey AS BetSettledStatus FROM DW.SettledStatus WHERE SettledStatusKey IN (3, 4, 5, 6, 7)) AS BetSettledStatus
 CROSS JOIN 
-    (SELECT DISTINCT BetSelectionTypeKey FROM DW.BetSelectionType) AS BetSelectionTypeKey
+(SELECT DISTINCT BetSelectionTypeKey FROM DW.BetSelectionType) AS BetSelectionTypeKey
 CROSS JOIN 
-    (SELECT DISTINCT BonusTypeKey FROM DW.BonusType) AS BonusTypeKey
+(SELECT DISTINCT BonusTypeKey FROM DW.BonusType) AS BonusTypeKey
 CROSS JOIN 
-    (SELECT DISTINCT CashoutParticipationKey AS CashoutParticipation FROM DW.VW_CashoutParticipation) AS CashoutParticipation
+(SELECT DISTINCT CashoutParticipationKey AS CashoutParticipation FROM DW.VW_CashoutParticipation) AS CashoutParticipation
