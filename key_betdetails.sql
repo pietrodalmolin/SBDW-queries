fe1022d226e9610bfe7aa68f10a9f5648007ac44
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-02-18 16:09:26 +0100

    Update key_betdetails.sql

--- key_betdetails.sql
+++ key_betdetails.sql
@@ -1,17 +1,33 @@
 SELECT 
-IsMobileBet
-,IsLiveBet
-,NumberOfSelections
-,CouponTypeKey
-,BetSettledStatus
-,BetSelectionTypeKey
-,BonusTypeKey
- 
+CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
+CONCAT(
+IsMobileBet, '|', 
+IsLiveBet, '|',
+NumberOfSelections, '|',
+CouponTypeKey, '|',
+BetSettledStatus, '|',
+BetSelectionTypeKey, '|',
+BonusTypeKey
+)
+), 2) AS key_betdetails,
+IsMobileBet,
+IsLiveBet,
+NumberOfSelections,
+CouponTypeKey,
+BetSettledStatus,
+BetSelectionTypeKey,
+BonusTypeKey
 FROM 
-(SELECT 0 AS IsMobileBet UNION ALL SELECT 1) AS IsMobileBet
-CROSS JOIN (SELECT 0 AS IsLiveBet UNION ALL SELECT 1) AS IsLiveBet
-CROSS JOIN (SELECT 1 AS NumberOfSelections UNION ALL SELECT 2) AS NumberOfSelections
-CROSS JOIN (SELECT DISTINCT CouponTypeKey FROM DW.CouponType) AS CouponTypeKey
-CROSS JOIN (SELECT DISTINCT SettledStatusKey AS BetSettledStatus FROM DW.SettledStatus WHERE SettledStatusKey IN (3, 4, 5, 6, 7)) AS BetSettledStatus
-CROSS JOIN (SELECT DISTINCT BetSelectionTypeKey FROM DW.BetSelectionType) AS BetSelectionTypeKey
-CROSS JOIN (SELECT DISTINCT BonusTypeKey FROM DW.BonusType) AS BonusTypeKey
+    (SELECT 0 AS IsMobileBet UNION ALL SELECT 1) AS IsMobileBet
+CROSS JOIN 
+    (SELECT 0 AS IsLiveBet UNION ALL SELECT 1) AS IsLiveBet
+CROSS JOIN 
+    (SELECT 1 AS NumberOfSelections UNION ALL SELECT 2) AS NumberOfSelections
+CROSS JOIN 
+    (SELECT DISTINCT CouponTypeKey FROM DW.CouponType) AS CouponTypeKey
+CROSS JOIN 
+    (SELECT DISTINCT SettledStatusKey AS BetSettledStatus FROM DW.SettledStatus WHERE SettledStatusKey IN (3, 4, 5, 6, 7)) AS BetSettledStatus
+CROSS JOIN 
+    (SELECT DISTINCT BetSelectionTypeKey FROM DW.BetSelectionType) AS BetSelectionTypeKey
+CROSS JOIN 
+    (SELECT DISTINCT BonusTypeKey FROM DW.BonusType) AS BonusTypeKey
