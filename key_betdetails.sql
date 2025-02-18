14f53c6bfbd055f9fbb78d664b9b34afd0a27dbb
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-02-18 15:41:37 +0100

    Update key_betdetails.sql

--- key_betdetails.sql
+++ key_betdetails.sql
@@ -1,38 +1,17 @@
-SELECT DISTINCT
-    -- Unique Key
-    CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', 
-        CONCAT(
-            COALESCE(IsMobileBet, ''), '|', 
-            COALESCE(IsLiveBet, ''), '|',
-            COALESCE(NumberOfSelections, ''), '|',
-            COALESCE(CouponTypeKey, ''), '|',
-            COALESCE(BetSettledStatus, ''), '|',
-            COALESCE(BetSelectionTypeKey, ''), '|',
-            COALESCE(BonusTypeKey, ''), '|',
-            COALESCE(CustomerClassificationAtBetPlacement, ''), '|',
-            COALESCE(BetStakeEURBucketKey, ''), '|',
-            COALESCE(SelectionsBucket, ''), '|',
-            COALESCE(CashoutParticipation, ''), '|',
-            COALESCE(NumberofPartialCashout, '')
-        )), 2) AS key_betdetails,
-
-    -- Dimensions for Power BI
-    IsMobileBet,
-    IsLiveBet,
-    NumberOfSelections,
-    CouponTypeKey,
-    BetSettledStatus,
-    BetSelectionTypeKey,
-    BonusTypeKey,
-    CustomerClassificationAtBetPlacement,
-    BetStakeEURBucketKey,
-    SelectionsBucket,
-    CashoutParticipation,
-    NumberofPartialCashout
-
-FROM MARTCUBE.BetSelectionFlat b WITH (NOLOCK) 
-
-WHERE 
-    BetSettledStatus IN (3, 4, 5, 6, 7)
-    AND b.AccountTypeKey = 1
-    AND reportingdate='2025-01-01'
+SELECT 
+IsMobileBet
+,IsLiveBet
+,NumberOfSelections
+,CouponTypeKey
+,BetSettledStatus
+,BetSelectionTypeKey
+,BonusTypeKey
+ 
+FROM 
+(SELECT 0 AS IsMobileBet UNION ALL SELECT 1) AS IsMobileBet
+CROSS JOIN (SELECT 0 AS IsLiveBet UNION ALL SELECT 1) AS IsLiveBet
+CROSS JOIN (SELECT 1 AS NumberOfSelections UNION ALL SELECT 2) AS NumberOfSelections
+CROSS JOIN (SELECT DISTINCT CouponTypeKey FROM DW.CouponType) AS CouponTypeKey
+CROSS JOIN (SELECT DISTINCT SettledStatusKey AS BetSettledStatus FROM DW.SettledStatus WHERE SettledStatusKey IN (3, 4, 5, 6, 7)) AS BetSettledStatus
+CROSS JOIN (SELECT DISTINCT BetSelectionTypeKey FROM DW.BetSelectionType) AS BetSelectionTypeKey
+CROSS JOIN (SELECT DISTINCT BonusTypeKey FROM DW.BonusType) AS BonusTypeKey
